4842fda3a62e714b1a0290d427e4f7c96b88e48a
RemoveYourSmile <danilgrickov311020@mail.ru>
2024-03-27 19:44:04 +0400

    1.1

--- src/ru/sgu/taskA.java
+++ src/ru/sgu/taskA.java
@@ -1,7 +1,40 @@
 package ru.sgu;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class taskA {
+
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        taskA streamInt = new taskA();
+        streamInt.printResult(x -> x > 5);
+    }
+
+    public IntStream read(){
+        while (true) {
+            try {
+                Scanner in = new Scanner(System.in);
+                int[] array = new int[10];
+                System.out.println("Введите 10 элементов:");
+                for (int i = 0; i < 10; i++) {
+                    array[i] = in.nextInt();
+                }
+                IntStream stream = Arrays.stream(array);
+                return stream;
+            }  catch (InputMismatchException e) {
+                System.out.println("Некорректный ввод");
+            }
+        }
+    }
+
+    public IntStream usingPredicate(IntStream stream, IntPredicate predicate){
+        return stream.filter(predicate);
+    }
+
+    public void printResult(IntPredicate predicate){
+        usingPredicate(this.read(), predicate).forEach(System.out::println);
     }
 }
--- src/ru/sgu/taskB.java
+++ src/ru/sgu/taskB.java
@@ -1,46 +1,70 @@
 package ru.sgu;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 
+
 public class taskB {
-    public static void main(String[] args) {
+    int max = -1000000000;
+    int min = 1000000000;
 
+    public static void main(String[] args){
+        taskB b = new taskB();
+        b.print();
+    }
+
+    List<Integer> readList() {
         while (true) {
-
             try {
-
+                List<Integer> list = new ArrayList<>();
                 Scanner in = new Scanner(System.in);
-                String str = in.nextLine();
-                String[] s = str.split(" ");
-                BigDecimal a = new BigDecimal(s[0]);
-                BigDecimal b = new BigDecimal(s[1]);
-
-                switch (s[2]) {
-                    case ("ADD"):
-                        System.out.println(a.add(b));
-                        break;
-                    case ("SUB"):
-                        System.out.println(a.subtract(b));
-                        break;
-                    case ("MULT"):
-                        System.out.println(a.multiply(b));
-                        break;
-                    case ("DIV"):
-                        System.out.println(a.divide(b));
-                        break;
-                    case ("REM"):
-                        System.out.println(a.remainder(b));
-                        break;
-                    case ("POW"):
-                        System.out.println(new BigInteger(s[0]).pow(Integer.parseInt(s[1])));
-                        break;
+                System.out.println("Введите размер массива: ");
+                int n = in.nextInt();
+                System.out.println("Введите " + n + " значений:");
+                for (int i = 0; i < n; i++) {
+                    list.add(i, in.nextInt());
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+                return list;
+            } catch (InputMismatchException e) {
+                System.out.println("Некорректный ввод");
             }
         }
+    }
+
+    int findMax(List<Integer> list){
+        if (list.size() < 2){
+            return 0;
+        }
+        max = list.stream().max(Integer::compare).get();
+        list.removeIf(x -> x == max);
+        if (list.size() == 0){
+            return max;
+        } else max = list.stream().max(Integer::compare).get();
+        return max;
+    }
+
+    int findMin(List<Integer> list){
+        if (list.size() < 3){
+            return 0;
+        }
+        for (int i = 0; i < 2; i++){
+            if (list.size() == 0){
+                break;
+            }
+            min = list.stream().min(Integer::compare).get();
+            list.removeIf(x -> x == min);
+        }
+        if (list.size() == 0){
+            return min;
+        } else min = list.stream().min(Integer::compare).get();
+        return min;
+    }
+
+    void print(){
+        System.out.println("2-е максимальное значение: " + findMax(this.readList()));
+        System.out.println("3-е минимальное значение: " + findMin(this.readList()));
     }
 }
